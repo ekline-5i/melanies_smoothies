a40f4ed258a1bb4973d2019e88fcb728ed5378fb
ekline-5i <elric.kline@fiveirongolf.com>
2025-01-07 22:50:50 -0500

    Update streamlit_app.py
    I want my badge even if the code is worse

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,10 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)
+    ingredients_string = ' '
 
     for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 
         st.subheader(fruit_chosen + ' Nutrition Information')
